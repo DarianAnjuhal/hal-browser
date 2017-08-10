73ec216bc9e22c96bb138eefa24d125c7a88a10d
Daniel <daniel.antlinger@alphaport.at>
2017-08-10 10:46:53 +0200

    try execute custom javascript on html side

--- js/hal.js
+++ js/hal.js
@@ -80,8 +80,4 @@
   };
 
   window.HAL = HAL;
-
-  //add by cmayr and dantlinger manually -> bad HOTFIX
-  $.ajaxSetup({ beforeSend: function (xhr) { xhr.setRequestHeader("Authorization",$('.request-headers textarea').val()); } });
-  console.log("Add custom Auth stuff to each ajax call -> HOTFIX");
 })();
