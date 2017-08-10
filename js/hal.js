55f773afbe7d45df150b27c788f429b94a62a856
Daniel <daniel.antlinger@alphaport.at>
2017-08-10 10:38:53 +0200

    add hotfix for authentication issue

--- js/hal.js
+++ js/hal.js
@@ -80,4 +80,8 @@
   };
 
   window.HAL = HAL;
+
+  //add by cmayr and dantlinger manually -> bad HOTFIX
+  $.ajaxSetup({ beforeSend: function (xhr) { xhr.setRequestHeader("Authorization",$('.request-headers textarea').val()); } });
+  console.log("Add custom Auth stuff to each ajax call -> HOTFIX");
 })();
